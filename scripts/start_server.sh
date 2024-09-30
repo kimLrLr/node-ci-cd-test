c5fb0bc3f3856a5ce5a85cc43ab3685625b3ad88
kimLrLr <rimiha9708@gmail.com>
2024-09-30 17:21:45 +0900

    배포 재시도

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@ export NVM_DIR="/home/ubuntu/.nvm"
 cd /home/ubuntu/node-app
 sudo chown -R ubuntu:ubuntu /home/ubuntu/node-app
 /home/ubuntu/.nvm/versions/node/v20.17.0/bin/npm install
-/home/ubuntu/.nvm/versions/node/v20.17.0/bin/pm2 start /home/ubuntu/node-app/src/server.js --name "node-app"
+/home/ubuntu/.nvm/versions/node/v20.17.0/bin/pm2 restart /home/ubuntu/node-app/src/server.js --name "node-app"
